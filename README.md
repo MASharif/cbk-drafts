7640600c9ebae7bb83924379e65ac890b2dd283c
E. Archer <Elaine@ElaineArcher.com>
2014-10-11 22:23:55 -0400

    First commit of css--border.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A demo of an idea to walk kids through structuring HTML documents. See it [here.
 
 ##Baskets
 A demo furthering the tag-testing idea, this time with link, image, and header tags. See it [here.](http://elainearcher.com/cbk/baskets.html)
+
+##CSS&#8212;Borders
+A self-directed slideshow skeleton with placeholders. See it [here.](http://elainearcher.com/cbk/css-border.html)
---
+++ js/css-border.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+
+  //Set variables
+  var slide2number = 1;
+  var slideNumber = 1;
+  var slideTotal = 5;
+
+  //Flip through border samples in slide 2.
+  function rotateImages(){
+    var current = $("#slide-2-image .active");
+    if (slide2number<5){
+      current.next().removeClass('hidden').addClass('active');
+      slide2number++;
+    }
+    else{
+      slide2number = 1;
+      current.parent().find(">:first-child").removeClass('hidden').addClass('active');
+    }
+    current.removeClass('active').addClass('hidden');
+  }
+  //Interval for image rotation.
+  setInterval(
+    function() 
+    {
+      rotateImages();
+    }, 1500
+  );//End interval
+
+  //Function for moving slides forward
+  function advanceSlide(){
+    var current = "#slide-"+slideNumber;
+    if (slideNumber==(slideTotal-1)){
+      $("#next").hide();
+      $(current).next().removeClass('hidden').addClass('active');
+      slideNumber++;
+    }
+    else if (slideNumber<slideTotal){
+      $(current).next().removeClass('hidden').addClass('active');
+      slideNumber++;
+    }
+    else{
+      $("#next").hide();
+      slideNumber = 1;
+      var current = "#slide-"+slideNumber;
+      $(current).removeClass('hidden').addClass('active');
+    }
+    $(current).removeClass('active').addClass('hidden');
+  }
+
+  //Function for moving slides backward
+  function reverseSlide(){
+    var current = "#slide-"+slideNumber;
+    if (slideNumber>2){
+      $(current).prev().removeClass('hidden').addClass('active');
+      slideNumber--;
+    }
+    else if (slideNumber==2){
+      $("#prev").hide();
+      $(current).prev().removeClass('hidden').addClass('active');
+      slideNumber--;
+    }
+    else{
+      $("#prev").hide();
+      slideNumber = 1;
+      var current = "#slide-"+slideNumber;
+      $(current).removeClass('hidden').addClass('active');
+    }
+    $(current).removeClass('active').addClass('hidden');
+  }
+
+  //Start over
+  $("#reset").on( "click", function() {
+    window.location.reload();
+  });
+
+  //Slider controls
+  $("#next").on("click", function() {
+    $("#prev").show();
+    advanceSlide();
+  });
+  $("#prev").on("click", function() {
+    $("#next").show();
+    reverseSlide();
+  });
+
+  //Add letter keypress on key, from http://stackoverflow.com/questions/1402698/binding-arrow-keys-in-js-jquery
+  $(document).keydown(function(e){
+    if (e.keyCode == 37) { 
+      $("#next").show();
+      reverseSlide();
+    }
+    if (e.keyCode == 39) { 
+      $("#prev").show();
+      advanceSlide();
+    }
+  });
+
+  //Page setup on load
+  $("#prev").hide();
+
+});
